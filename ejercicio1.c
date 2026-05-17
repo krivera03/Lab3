63ed2d9727d637788f84b87e0529648b97891681
Karen Rivera <k.riveraangulo@outlook.com>
2026-05-17 17:13:48 -0600

    Generación de branches y colocación de código para calcular potencias enteras

---
+++ ejercicio1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int potencia(int base, int exp) {
+    int resultado = 1;
+    while (exp > 0) {
+        resultado = resultado * base;
+        int exp = exp - 1;
+    }
+    return resultado;
+}
+
+int main(void) {
+    printf("2^8 = %d\n", potencia(2, 8));
+    printf("3^4 = %d\n", potencia(3, 4));
+    return0;
+}
