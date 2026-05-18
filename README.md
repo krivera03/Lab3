fcbc2e266df3a321c5b5b988f763cea6e037be59
Karen Rivera <k.riveraangulo@outlook.com>
2026-05-17 18:23:12 -0600

    Creación del README para la documentación del laboratorio. Se elimina el archivo ejercicio1 que no contenía información

---
+++ README.md
@@ -0,0 +1,14 @@
+# Laboratorio 3
+Karen Rivera Angulo 
+
+C09197
+
+---
+
+## Ejercicio 1
+
+
+
+## Ejercicio 2
+
+## Ejercicio 3
--- ejercicio1.c
+++ ejercicio1.c
@@ -12,5 +12,5 @@ int potencia(int base, int exp) {
 int main(void) {
     printf("2^8 = %d\n", potencia(2, 8));
     printf("3^4 = %d\n", potencia(3, 4));
-    return0;
+    return 0;
 }
