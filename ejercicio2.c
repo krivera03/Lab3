9b4897aa68bc9b97a2dff43402b3321d1b50af38
Karen Rivera <k.riveraangulo@outlook.com>
2026-05-18 02:00:44 -0600

    Se agregan las funciones que verifican si tiene una fila completa de unos o una columna completa de unos

--- ejercicio2.c
+++ ejercicio2.c
@@ -12,14 +12,14 @@ int tiene_columna_completa(int m[][SIZE]);
 int main(void) {
     int m[SIZE][SIZE] = {
         {1, 0, 0, 0},
-        {0, 1, 0 ,0},
-        {0, 0, 1, 0},
-        {0, 0, 0, 1}
+        {1, 1, 0 ,0},
+        {1, 0, 1, 0},
+        {1, 0, 0, 1}
     };
 
     int i, j;
 
-    printf("Matriz ingresada:\n");
+    printf("Matriz ingresada:\n");  //Imprime matriz ingresada
 
     for (i = 0; i < SIZE; i++) {
 
@@ -31,13 +31,29 @@ int main(void) {
         printf("\n");
     }
 
+    //Imprime la cuenta de unos en la fila 1, columna 1
     printf("Unos en la fila: %d\n", contar_unos_fila(m, 0));
     printf("Unos en la columna: %d\n", contar_unos_columna(m, 1));
 
+    // Imprime si la matriz es identidad
     if (es_identidad(m)) {
         printf("Es matriz identidad: si\n");
     } else {
         printf("Es matriz identidad: no\n");
+    }
+
+    // Imprime si tiene una fila completa de unos
+    if (tiene_fila_completa(m)) {
+        printf("Tiene fila completa de unos: si\n");
+    } else {
+        printf("Tiene fila completa de unos: no\n");
+    }
+
+    // Imprime si tiene una columna completa de unos
+    if (tiene_columna_completa(m)) {
+        printf("Tiene columna completa de unos: si\n");
+    } else {
+        printf("Tiene columna completa de unos: no\n");
     }
 
     return 0;
@@ -102,4 +118,55 @@ int es_identidad(int m[][SIZE]) {
     }
 
     return 1;
+}
+
+
+// Verifica si hay una fila completa de unos
+int tiene_fila_completa(int m[][SIZE]) {
+
+    int i, j;
+    int completa;
+
+    for (i = 0; i < SIZE; i++) {
+
+        completa = 1;
+
+        for (j = 0; j < SIZE; j++) {
+
+            if (m[i][j] != 1) {
+                completa = 0;
+            }
+        }
+
+        if (completa == 1) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+//Verifica si tiene una columna completa de unos
+int tiene_columna_completa(int m[][SIZE]) {
+
+    int i, j;
+    int completa;
+
+    for (j = 0; j < SIZE; j++) {
+
+        completa = 1;
+
+        for (i = 0; i < SIZE; i++) {
+
+            if (m[i][j] != 1) {
+                completa = 0;
+            }
+        }
+
+        if (completa == 1) {
+            return 1;
+        }
+    }
+
+    return 0;
 }
