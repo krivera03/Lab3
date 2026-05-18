6d349da1aa730ca806a1510e605b1f82426adfb7
Karen Rivera <k.riveraangulo@outlook.com>
2026-05-18 00:52:38 -0600

    Se agrega el código que cuenta cuantos unos hay en una fila y en una columna, corrigiendo errores de sintaxis y de llamados a la función

---
+++ ejercicio2.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 4
+
+int contar_unos_fila(int m[][SIZE], int fila);
+int contar_unos_columna(int m[][SIZE], int col);
+int es_identidad(int m [][SIZE]);
+int tiene_fila_completa(int m[][SIZE]);
+int tiene_columna_completa(int m[][SIZE]);
+
+int main(void) {
+    int m[SIZE][SIZE] = {
+        {1, 0, 0, 0},
+        {0, 1, 0 ,0},
+        {0, 0, 1, 0},
+        {0, 0, 0, 1}
+    };
+
+    printf("Unos en la fila: %d\n", contar_unos_fila(m, 0));
+    printf("Unos en la columna: %d\n", contar_unos_columna(m, 1));
+
+    return 0;
+
+}
+
+
+// Contar cuantos unos hay en una fila
+int contar_unos_fila(int m[][SIZE], int fila) {
+    
+    int i;
+    int contador = 0;  //Inicia el contador en 0
+
+    for (i = 0; i < SIZE; i++) {  //Recorre todas la columnas de la fila
+        if (m[fila][i] == 1) { 
+            contador ++;
+        }
+    }
+
+    return contador;
+}
+
+// Contar cuantos unos hay en una columna, se repite el proceso
+int contar_unos_columna(int m[][SIZE], int col) {
+    
+    int i;
+    int contador = 0;  //Inicia el contador en 0
+
+    for (i = 0; i < SIZE; i++) {
+        if (m[i][col] == 1) { 
+            contador ++;
+        }
+    }
+
+    return contador;
+}
