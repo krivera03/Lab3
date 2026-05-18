0f82a26b52098506b5d52c15b5d970d5dc4db2e1
Karen Rivera <k.riveraangulo@outlook.com>
2026-05-18 17:00:27 -0600

    Se añade el código que limita los rangos de los sensores. Se corrigen errores de sintaxis, eliminar condición del else y colocar el main para que compile adecuadamente

--- ejercicio3.c
+++ ejercicio3.c
@@ -3,19 +3,40 @@
 
 typedef enum { NORMAL, ALERTA, FALLO} EstadoSensor;
 
+// Lectura del sensor
 typedef struct
 {
     int id;
     float valor;
-    EstadoSensor estado;
+    EstadoSensor estado;  //Clasifica utilizando el enum
 } Lectura;
 
-EstadoSensor clasificar (float valor);
-int     contar_estado(Lectura s [] , int n, EstadoSensor e);
-int     lectura_extrema(Lectura s[] , int n);
+// Clasificar valores según los rangos
+EstadoSensor clasificar (float valor) { 
 
-int main(void) {
+    if (valor >= 40.0 && valor <= 60.0)
+        return NORMAL;
+    else if ((valor >=20.0 && valor < 40.0) || (valor <= 60.0 && valor > 80.0))
+        return ALERTA;
+    else 
+        return FALLO;
+}
+    
+//Imprimir estado de los sensores
+void imprimir_estado(EstadoSensor e){
+    if (e == NORMAL) 
+        printf("Normal");
+    else if (e == ALERTA) 
+        printf("Alerta");
+    else 
+        printf("Fallo");
+}
 
+int main(void){
 
     return 0;
 }
+
+//int     contar_estado(Lectura s [] , int n, EstadoSensor e);
+//int     lectura_extrema(Lectura s[] , int n);
+
