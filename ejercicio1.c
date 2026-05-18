f66f9a1f973ed9633f7e236bae265e2eba1db34a
Karen Rivera <k.riveraangulo@outlook.com>
2026-05-17 22:16:47 -0600

    Se modifica el código para que reconozca bien las variables y se corrigen errores de sintaxis. El código funciona correctamente

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,29 +1,9 @@
 #include <stdio.h>
 
-int main(){
-    int base, exp;
 
-    do {
-        printf("Ingrese la base de la potencia: ");
-        scanf("%d\n", &base);
-
-    } while (base == 0);
-        printf("Número inválido. Ingrese una base diferente de cero: ")
-        scanf("%d\n", &base)
-
-    do {
-        printf("Ingrese el exponente de la potencia: ")
-        scanf("%d\n", &exp)
-
-    } while (exp > 0);
-        printf("Número inválido. Ingrese un exponente positivo: ")
-        scanf("%d\n", &exp)
-
-    return 0;
-}
-
-int potencia(base, exp) {
+int potencia(int base, int exp) { 
     int resultado = 1;
+
     while (exp > 0) {
         resultado = resultado * base;
         exp = exp - 1;     // Se elimina el int que generaba una nueva variable
@@ -31,7 +11,32 @@ int potencia(base, exp) {
     return resultado;
 }
 
-int main(void) {
-    printf("%d\n^%d\n = %d\n", base, exp, resultado);
+int main(void){ 
+    int base, exp;
+ 
+    // Ingresar y validar base distinta de cero
+    do {
+        printf("Ingrese la base de la potencia: ");
+        scanf("%d", &base);
+        
+        if (base == 0) { 
+        printf("Número inválido, la base debe ser diferente de cero. ");
+        }
+    } while (base == 0);
+
+    // Ingresar y validar exponente mayor a cero
+    do {
+        printf("Ingrese el exponente de la potencia: ");
+        scanf("%d", &exp);
+
+        if (exp < 0) {
+            printf("Número inválido, el exponente debe ser mayor a cero. ");
+        }
+
+    } while (exp < 0);
+   
+    // Imprime el resultado de la potencia
+    printf("%d^%d = %d\n", base, exp, potencia(base, exp));
+
     return 0;
 }
