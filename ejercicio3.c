06cf256da02f4e0b14b581ef37da2e7555520767
Karen Rivera <k.riveraangulo@outlook.com>
2026-05-18 21:46:26 -0600

    Se añade al código la lectura de los ID de los sensores y los valores. Asi como la imagen que muestra los resultados de la misma

--- ejercicio3.c
+++ ejercicio3.c
@@ -34,8 +34,36 @@ void imprimir_estado(EstadoSensor e){
 
 int main(void){
 
+        Lectura s[MAX_SENSORES];
+        int n = MAX_SENSORES;
+
+        //Lecturas del usuario
+        for (int i = 0; i < n; i++){
+            printf("Ingrese el ID del sensor %d: ", i + 1);
+            scanf("%d", &s[i].id);
+
+            printf("Ingrese el valor del sensor %d: ", i + 1);
+            scanf("%f", &s[i].valor);
+
+            //Clasificar 
+            s[i].estado = clasificar(s[i].valor);
+        }
+
+        //Tabla de resultado
+        printf("\n--- Tabla de Lecturas ---\n");
+        printf("ID\tValor\tEstado\n");
+
+        for (int i = 0; i < n; i++){
+            printf("%d\t%.2f\t", s[i].id, s[i].valor);
+            imprimir_estado(s[i].estado);
+            printf("\n");
+        }
+    
     return 0;
 }
+
+
+
 
 //int     contar_estado(Lectura s [] , int n, EstadoSensor e);
 //int     lectura_extrema(Lectura s[] , int n);
